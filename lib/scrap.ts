9483e8320b3ba4f4b04a7982f17d67ced4d29744
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2023-07-21 15:42:15 +0530

    Update scraping operation to retrieve links

--- lib/scrap.ts
+++ lib/scrap.ts
@@ -3,23 +3,34 @@ import { load } from 'cheerio'
 
 const url = 'https://globalnews.ca/'
 
-export async function scrapTitle(): Promise<string[]> {
-	let titles: string[] = []
+export interface NewsItem {
+	headline: string
+	link: string
+}
+
+export async function scrapTitle(): Promise<NewsItem[]> {
+	let newsItems: NewsItem[] = []
 	try {
 		const response = await axios.get(url)
 		if (response.status === 200) {
 			const html: string = response.data
 			const $ = load(html)
 			// Performing scraping operations
-			$('.c-posts__headlineText').each((index, element) => {
-				const title: string = $(element).text()
-				titles.push(title)
+			$('.c-posts__item').each((index, element) => {
+				const headline: string | undefined = $(element)
+					.find('span.c-posts__headlineText')
+					.attr('title')
+				const link: string | undefined =
+					$(element).find('a.c-posts__inner').attr('href') ??
+					$(element).find('a.c-posts__headlineLink').attr('href')
+
+				if (headline && link) newsItems.push({ headline, link })
 			})
 		}
 	} catch (error) {
 		console.error('Error: ', error)
 	}
-	return titles
+	return newsItems
 }
 
 module.exports = {
