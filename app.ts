f606cf9c1edab21b29a3bc5dc4b7c6e8a74831d9
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2023-07-21 15:42:30 +0530

    Update types

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from 'express'
-import { scrapTitle } from './lib/scrap'
+import { scrapTitle, NewsItem } from './lib/scrap'
 
 const app = express()
 const port = 3000
 
 app.get('/', async (req: Request, res: Response) => {
 	try {
-		const titles: string[] = await scrapTitle()
-		res.status(200).send(titles)
+		const newsItems: NewsItem[] = await scrapTitle()
+		res.status(200).send(newsItems)
 	} catch (error) {
 		console.error('Error', error)
 		res.status(500).send('Internal Server Error')
