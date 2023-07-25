f6436952f19af9e66dddc12af08eaaac8b6b10b2
Aashish <takoaashish01@gmail.com>
2023-07-25 18:23:55 -0500

    Made client-side and server-side

---
+++ client/src/components/ListGroup.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function ListGroup() {
+  const items = ["Car", "Dog", "Elephant", "Deer"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active ?"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
