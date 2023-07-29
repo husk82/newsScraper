0dba96a51e206d70be860d5612d06e8b37da0fc1
Aashish <takoaashish01@gmail.com>
2023-07-28 22:18:30 -0500

    Optimized wordMatcher.

--- server/lib/wordMatcher.ts
+++ server/lib/wordMatcher.ts
@@ -1,20 +1,14 @@
 import { STOP_WORDS } from "./stopWords";
 
-function calculateWordSimilarity(title1: string, title2: string): number {
-  const words1 = title1
-    .toLowerCase()
-    .split(/\s+/)
-    .filter((word) => !STOP_WORDS.has(word));
-  const words2 = title2
-    .toLowerCase()
-    .split(/\s+/)
-    .filter((word) => !STOP_WORDS.has(word));
-
-  const totalWords = words1.length + words2.length;
+function calculateWordSimilarity(
+  words1: Set<string>,
+  words2: Set<string>
+): number {
+  const totalWords = words1.size + words2.size;
   let matchedWords = 0;
 
-  words1.forEach((word1) => {
-    if (words2.includes(word1)) {
+  words1.forEach((word) => {
+    if (words2.has(word)) {
       matchedWords++;
     }
   });
@@ -33,11 +27,36 @@ function findSimilarTitles(
     similarity: number;
   }[] = [];
 
-  array1.forEach((title1) => {
-    array2.forEach((title2) => {
-      const similarity = calculateWordSimilarity(title1, title2);
+  const titleWords1: Set<string>[] = array1.map(
+    (title) =>
+      new Set(
+        title
+          .toLowerCase()
+          .split(/\s+/)
+          .filter((word) => !STOP_WORDS.has(word))
+      )
+  );
+
+  const titleWords2: Set<string>[] = array2.map(
+    (title) =>
+      new Set(
+        title
+          .toLowerCase()
+          .split(/\s+/)
+          .filter((word) => !STOP_WORDS.has(word))
+      )
+  );
+
+  // Compare the titles based on word similarity
+  titleWords1.forEach((words1, index1) => {
+    titleWords2.forEach((words2, index2) => {
+      const similarity = calculateWordSimilarity(words1, words2);
       if (similarity >= similarityThreshold) {
-        similarTitles.push({ title1, title2, similarity });
+        similarTitles.push({
+          title1: array1[index1],
+          title2: array2[index2],
+          similarity,
+        });
       }
     });
   });
